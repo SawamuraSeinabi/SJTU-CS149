6e4d64117f7427337743b09d4eedf23761508d3b
澤村セイナビ <sawamuraseinabi@sjtu.edu.cn>
2020-02-13 23:47:58 +0800

    AVL树

---
+++ AVLTree/dynamicsearchtable.h
@@ -0,0 +1,15 @@
+#pragma once
+template <class KEY, class OTHER>
+struct SET
+{
+	KEY key;
+	OTHER other;
+};
+template <class KEY, class OTHER>
+class dynamicSearchTable {
+public:
+	virtual SET<KEY, OTHER>* find(const KEY& x) const = 0;
+	virtual void insert(const SET<KEY, OTHER>& x) = 0;
+	virtual void remove(const KEY& x) = 0;
+	virtual ~dynamicSearchTable() {};
+};
