b457cd4de7775e362e7f68e096fda618ee0a4943
澤村セイナビ <sawamuraseinabi@sjtu.edu.cn>
2020-02-13 23:49:39 +0800

    Revert "哈希表"
    This reverts commit 37f0f5863d0cd2cbe2e44f944dc6ff2e590bf48e.

--- HashTable/HashTable.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-template <class Type>class hashtable{
-public:	virtual bool find(const Type& x)const = 0;	virtual bool insert(const Type& x) = 0;	virtual bool remove(const Type& x) = 0;protected:	int(*key)(const Type& x);	static int defaultKey(const int& k) { return k; }};
