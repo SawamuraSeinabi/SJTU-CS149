37f0f5863d0cd2cbe2e44f944dc6ff2e590bf48e
澤村セイナビ <sawamuraseinabi@sjtu.edu.cn>
2020-02-13 23:47:49 +0800

    哈希表

---
+++ HashTable/HashTable.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+#include "HashTable.h"
+#include "closeHashTable.h"
+int main()
+{
+	closeHashTable<int>ht(25013);
+	int count;
+	double avgCountSer = 0;
+	int list[10000];
+
+	srand(time(NULL));
+	for (int i = 0; i < 10000; i++)
+	{
+		list[i] = rand();
+		ht.insert2(list[i], count);
+		avgCountSer += count;
+	}
+	avgCountSer /= 10000;
+	cout << "平均探测次数：" << avgCountSer << endl;
+	return 0;
+}
